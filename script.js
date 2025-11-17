5002c142a8f754b430b7d830460aa483aa8bc8c6
AnavayCano <anavay.cano77@gmail.com>
2025-11-17 17:42:51 +0000

    input button working

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 console.log("Script started");
 
+function addTaskName(){
+    // Get task input
+    let taskInput = document.getElementById("task-prompt");
+    // Get input value
+    let task = taskInput.value;
+    // Get task title element
+    let taskTitle = document.getElementById("task-title");
+    console.log(taskTitle);
+    // Get inner text
+    taskTitle.innerText = task;
+}
