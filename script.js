afbf321c992f4c01973cb2fc41d16778bdf1398b
AnavayCano <anavay.cano77@gmail.com>
2025-11-24 17:43:12 +0000

    25 minute timer working

--- script.js
+++ script.js
@@ -23,26 +23,40 @@ function createFlashcards() {
 // timer
 let startSeconds = 25*60;
 
-function startTimer(startSeconds) {
+let timer = setInterval(function() {
     console.log("timer started");
 
-    // Get timer display
-    let timerDisplay = document.getElementById("time");
-    console.log(timerDisplay);
-    
-}
-
-function countdown() {
+    // Minus one every second
     startSeconds--;
 
-    let sessionsCompleted = [];
-    let breakCount = document.getElementById("break-count");
+    // Convert seconds to MM:SS format
+    let minutes = Math.floor(startSeconds/60);
+
+    let seconds = startSeconds % 60;
+
+    
+    // Get timer display
+    let timerDisplay = document.getElementById("time");
+
+    // Update timer display
+    timerDisplay.innerHTML = minutes + ":" + seconds;
+
+    if(seconds <= 9) {
+        timerDisplay.innerHTML = minutes + ":0" + seconds;
+    }
+
+    // Keep count of breaks and update display
+    countBreaks();
+    clearInterval(timer);
+    
+}, 1000)
+
+function countBreaks() {
+    let sessionsCompleted = 0;
+    let breakCount = document.getElementById("break-count").innerHTML;
     if(startSeconds == 0) {
         sessionsCompleted++;
-        breakCount.innerHTML = sessionsCompleted;
+        breakCount= sessionsCompleted;
     }
-}
-
-function changeDisplay() {
-
+    console.log(breakCount);
 }
